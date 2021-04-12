5ed10df0bff9938be07ea6db534ec1b024d57ec0
miigdm <migonzalez5@uc.cl>
2021-04-12 12:21:14 -0400

    initial commi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../stylesheets/application"
 
+import "app/assets/stylesheets/application.scss"
 
 Rails.start()
 Turbolinks.start()
